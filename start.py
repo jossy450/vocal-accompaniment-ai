707aa2f56b2d30d9df3294ed6b4e5eb85c6b64fd
Joseph Ojediran <31350956+jossy450@users.noreply.github.com>
2025-11-06 00:43:39 +0000

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
+# start.py
 import os
 import uvicorn
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', '8000'))
-    uvicorn.run('app:app', host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    # Railway will set PORT in the environment, default to 8000 locally
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
